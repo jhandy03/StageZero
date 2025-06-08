4df53c3f83d185fa17d346e1fc6ea080c0c11b68
Jordan Handy <jhandy03@vt.edu>
2025-06-08 15:32:10 -0400

    File removal & code updates
    Removed older files. Ported over useful functionality from older files into final file. Renamed to 'Engine_Design'. Added ability to use default values or inputs own parameters. Updated mainCalculations function to pass in variables

--- Engine_Design.py
+++ Engine_Design.py
@@ -1,3 +1,21 @@
+"""
+
+Author(s): Jordan Handy
+
+Last Updated: 06/08/2025
+
+Description:
+    Performs sizing and flow rate calculations for the StageZero BLANK Engine
+    
+Feature List:
+    Optimization - might want to do through RPA instead of doing it ourselves. If interested
+        look at Gurobi on the software website. Will likely need: import gurobipy as gb
+    Fusion Integration - needs to be done through Fusion itself. The coding side is a 
+        little weird
+    GUI?
+
+"""
+
 import matplotlib.pyplot as plt
 import math
 
@@ -7,9 +25,7 @@ def dataHandling():
     Isp_660 = []
     with open('SZ_Engine_CEA_Data.txt', 'r') as file:
         lines = file.readlines()
-        # Skip header (assume first line is header)
-        data_lines = lines[1:]
-        for line in data_lines:
+        for line in lines:
             parts = line.split()
             if len(parts) < 3:
                 continue
@@ -40,25 +56,21 @@ def OptimalOF(data):
         OFoptimal = OF[max_isp_index]
         max_isp = max_isp_600
         c = max_isp
+        max_isp = max_isp / 9.81 #m/s -> sec
         pressure_condition = "600 psi"
     else:
         max_isp_index = Isp_660.index(max_isp_660)
         OFoptimal = OF[max_isp_index]
         max_isp = max_isp_660
         c = max_isp
+        max_isp = max_isp / 9.81 #m/s -> sec
         pressure_condition = "660 psi"
-    
-    return OFoptimal, max_isp/9.81, pressure_condition, c
+    return OFoptimal, max_isp, pressure_condition, c
 
-def mainCalculations():
-    EngineName = 'Vulcan'
-    runtime = 10 # seconds
+def mainCalculations(EngineName,runtime,Thrust,Pc,Pa,FuelType,OxidizerType,
+                     cstar,AeAt,AcAt,Lstar,rhof,rhoox,DisCoef,
+                     OrificeCountf,OrificeCountox,):
     print(f"----------------------{EngineName}----------------------")
-    Thrust = 5000
-    Pc = 600
-    Pa = 95000
-    FuelType = 'Ethanol'
-    OxidizerType = 'Nitrous Oxide'
     OFoptimal, max_isp, pressure_condition, c = OptimalOF(dataHandling())
     # print('From the data given:')
     print(f'OF:                                 {OFoptimal:.3f}')
@@ -66,36 +78,27 @@ def mainCalculations():
     print(f'Chamber Pressure:                   {Pc} psi')
     print(f'Ambient Pressure:                   {Pa} Pa')
     
-    cstar = 1419.2 #m/s
-    AeAt = 6.1825
-    mdot_total = Thrust / c
-    mdot_fuel = mdot_total / (OFoptimal + 1)
-    mdot_oxidizer = mdot_fuel * OFoptimal
-    At = (cstar*mdot_total) / (Pc * 6894.76)  # Convert Pc from psi to Pa
-    Dt = (4 * At / math.pi) ** 0.5
-    Ae = AeAt * At
-    De = (4 * Ae / math.pi) ** 0.5
+    mdot_total = Thrust/c
+    mdot_fuel = mdot_total/(OFoptimal + 1)
+    mdot_oxidizer = mdot_fuel*OFoptimal
+    At = (cstar*mdot_total)/(Pc*6894.76)  #6894.75: psi -> Pa
+    Dt = (4*At/math.pi)**0.5
+    Ae = AeAt*At
+    De = (4*Ae/math.pi)**0.5
     
-    Pinjection = 1.3*Pc*6894.76  #TODO: Not sure if this value is correc. revisit later
-    rhof = 789.3  #TODO: Update value
-    rhoox = 1125.0  #TODO: Update value
-    DisCoef = 0.6  # Discharge coefficient (Maybe Update?)
+    Pinjection = 1.3*Pc*6894.76  #TODO: Not sure if this value is correct. revisit later (6894.75: psi -> Pa)
+    
     FuelArea = mdot_fuel/(DisCoef*(math.sqrt(2*rhof*(Pinjection-Pc))))
-    OrificeCountf = 20 #TODO: Update value
-    OrificeAreaf = FuelArea / OrificeCountf
-    OrificeDiamf = (4 * OrificeAreaf / math.pi) ** 0.5
+    OrificeAreaf = FuelArea/OrificeCountf
+    OrificeDiamf = (4*OrificeAreaf/math.pi)**0.5
     
     OxiArea = mdot_oxidizer/(DisCoef*(math.sqrt(2*rhoox*(Pinjection-Pc))))
-    OrificeCountox = 20 #TODO: Update value
-    OrificeAreaox = OxiArea / OrificeCountox
-    OrificeDiamox = (4 * OrificeAreaox / math.pi) ** 0.5
+    OrificeAreaox = OxiArea/OrificeCountox
+    OrificeDiamox = (4*OrificeAreaox/math.pi)**0.5
     
-    AcAt = 5 #TODO: Update value
     Ac = AcAt*At
-    Dc = (4 * Ac / math.pi) ** 0.5
-    Lstar = 0.5 #TODO: Update value
+    Dc = (4*Ac/math.pi)**0.5
     Lc = Lstar/AcAt
-    
     massfuel = mdot_fuel * runtime
     massoxidizer = mdot_oxidizer * runtime
 
@@ -133,5 +136,44 @@ def mainCalculations():
 
  
 if __name__ == "__main__":
-    mainCalculations()
+    print('Use default parameters? (y/n)')
+    default = input().strip().lower()
+    if default == 'y':
+        EngineName = 'Vulcan' #TODO: Update value
+        runtime = 10 # sec
+        Thrust = 5000 #N
+        Pc = 600 #psi
+        Pa = 95000 #Pa
+        FuelType = 'Ethanol'
+        OxidizerType = 'Nitrous Oxide'
+        cstar = 1419.2 #m/s
+        AeAt = 6.1825
+        rhof = 789.3  #TODO: Update value
+        rhoox = 1125.0  #TODO: Update value
+        DisCoef = 0.6  # Discharge coefficient (Maybe Update?)
+        OrificeCountf = 20 #TODO: Update value
+        OrificeCountox = 20 #TODO: Update value
+        AcAt = 5 #TODO: Update value
+        Lstar = 0.5 #TODO: Update value
+    else:
+        EngineName = input('Enter Engine Name: ')
+        runtime = float(input('Runtime (sec): '))
+        Thrust = float(input('Thrust (N): '))
+        Pc = float(input('Chamber Pressure (psi): '))
+        Pa = float(input('Ambient Pressure (Pa): '))
+        FuelType = input('Fuel: ')
+        OxidizerType = input('Oxidizer: ')
+        cstar = float(input('Characteristic Velocity (m/s): '))
+        AeAt = float(input('Ae/At: '))
+        rhof = float(input('Fuel Density (kg/m^3): '))
+        rhoox = float(input('Oxidizer Density (kg/m^3): '))
+        DisCoef = float(input('Discharge Coefficient: '))
+        OrificeCountf = int(input('Number of Fuel Holes: '))
+        OrificeCountox = int(input('Number of Oxidizer Holes: '))
+        AcAt = float(input('Ac/At: '))
+        Lstar = float(input('L*: '))
+
+    mainCalculations(EngineName, runtime, Thrust, Pc, Pa, FuelType, OxidizerType,
+                     cstar, AeAt, AcAt, Lstar, rhof, rhoox, DisCoef,
+                     OrificeCountf, OrificeCountox)
     
